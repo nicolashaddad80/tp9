b682477b521a9269b1abe6937b414928c21136d5
Tassadit <sadountassadit@yahoo.fr>
2020-01-17 19:44:26 +0100

    FirstCommit

--- EditorTest.java
+++ EditorTest.java
@@ -4,12 +4,15 @@ package fr.cnam.tp9;
 public class EditorTest {
 
 
-
-
+    /**
+     * Test Editor
+     * @param Args
+     */
     public static void main(String[] Args)
     {
         Editor myEditor1;
         myEditor1=new Editor();
         myEditor1.editer();
+
     }
 }
