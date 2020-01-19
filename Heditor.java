66fe495b3492c2d006fc6f6047f56e8bcb1789cf
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-01-19 22:08:08 +0100

    Version 3.0 Release condidate

--- Heditor.java
+++ Heditor.java
@@ -1,19 +1,16 @@
 package fr.cnam.tp9;
 
 public class Heditor extends Editor implements HisEditor {
+
     private CommandHistory undoHistory;
     private CommandHistory redoHistory;
 
-    private UndoCommand undoCommand=new UndoCommand(this);
-    private RedoCommand redoCommand=new RedoCommand(this);
-
-    public Heditor(Line a_Line){
+    public Heditor(Line a_Line) {
         super(a_Line);
-        this.undoHistory =new LineCommandHistory();
-        this.redoHistory =new LineCommandHistory();
-        this.currentMenu.add(new Entry("Undo",undoCommand,"-"));
-        this.currentMenu.add(new Entry("Redo",redoCommand,"+"));
-
+        this.undoHistory = new LineCommandHistory();
+        this.redoHistory = new LineCommandHistory();
+        this.currentMenu.add(new Entry("Undo", new UndoCommand(this), "-"));
+        this.currentMenu.add(new Entry("Redo", new RedoCommand(this), "+"));
     }
 
     @Override
@@ -29,39 +26,30 @@ public class Heditor extends Editor implements HisEditor {
     @Override
     public void redo() {
 
-        Line previousLine=this.redoHistory.pull();
+        Line previousLine = this.redoHistory.pull();
         this.undoHistory.push(this.line.clone());
         this.restoreLine(previousLine);
-
-
-
-
     }
 
     @Override
     public void undo() {
-        Line previousLine=this.undoHistory.pull();
+        Line previousLine = this.undoHistory.pull();
         this.redoHistory.push(this.line.clone());
         this.restoreLine(previousLine);
-
-
-
-
     }
 
 
     @Override
     protected void execute(Command a_Command) {
-        if(a_Command.isCancellable()){
-                this.undoHistory.push (this.line.clone());
+        if (a_Command.isCancellable()) {
+            this.undoHistory.push(this.line.clone());
 
         }
         super.execute(a_Command);
     }
 
-    protected void restoreLine(Line a_Line){
-        this.line=a_Line;
-
-        for(int i=0;i<this.nbLineCommands;i++) this.lineCommandsTable[i].setLine(a_Line);
+    private void restoreLine(Line a_Line) {
+        this.line = a_Line;
+        for (int i = 0; i < this.nbLineCommands; i++) this.lineCommandsTable[i].setLine(a_Line);
     }
 }
