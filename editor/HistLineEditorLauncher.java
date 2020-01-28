4f4ea566f60e7087f718d79618ea960c3eedb47a
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-01-28 02:52:48 +0100

    Undo Implemented Working ok Undo V1.0

--- editor/HistLineEditorLauncher.java
+++ editor/HistLineEditorLauncher.java
@@ -1,10 +1,12 @@
 package fr.cnam.tp9.editor;
 
 import fr.cnam.tp9.command.specification.Cancelable;
+
 import fr.cnam.tp9.editor.specification.HisEditor;
 import fr.cnam.tp9.line.ClonableLineTab;
-import fr.cnam.tp9.line.linecommands.canceLincomm.CanceAddBeginningCommand;
-import fr.cnam.tp9.line.linecommands.canceLincomm.CancelableLinComm;
+import fr.cnam.tp9.line.linecommands.canceLincomm.*;
+
+import fr.cnam.tp9.line.linecommands.simplelinComm.*;
 import fr.cnam.tp9.line.specification.ClonableLine;
 import fr.cnam.tp9.menu.EntryImp;
 import fr.cnam.tp9.menu.MenuImp;
@@ -42,51 +44,49 @@ public class HistLineEditorLauncher {
 
         //filling the Menu
         int i = 0, j = 0, c = 0, nbLineCommands = 0;
-        Entry[] mainMenuList = new Entry[1];
-     //   Entry[] subMenuList = new Entry[3];
+        Entry[] mainMenuList = new Entry[8];
+        Entry[] subMenuList = new Entry[3];
 
         //Creating Commands:
 
+        lineCommandsTable[nbLineCommands++] = new CanceMoveBeginningCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceMoveRightCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceMoveLeftCommand(this.myClonableLineTab);
         lineCommandsTable[nbLineCommands++] = new CanceAddBeginningCommand(this.myClonableLineTab);
-        //
-         /*   lineCommandsTable[nbLineCommands++] = new MoveRightCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new MoveLeftCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new AddBeginningCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new AddEndCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new ReplaceCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new DeleteCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new AddBeforeCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new AddAfterCommand(this.myClonableLineTab);
-            lineCommandsTable[nbLineCommands++] = new DeleteAllCommand(this.myClonableLineTab);
-*/
-            //Creating and filling CursorSubMenu
+        lineCommandsTable[nbLineCommands++] = new CanceAddEndCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceReplaceCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceDeleteCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceAddBeforeCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceAddAfterCommand(this.myClonableLineTab);
+        lineCommandsTable[nbLineCommands++] = new CanceDeleteAllCommand(this.myClonableLineTab);
+        
 
-/*
-            subMenuList[i++] = new EntryImp(i, "Placer le curseur au debut de la ligne    ", lineCommandsTable[c++], "o");
-            subMenuList[i++] = new EntryImp(i, "Avancer le curseur d une position a droite", lineCommandsTable[c++], "l");
-            subMenuList[i++] = new EntryImp(i, "Reculer le curseur d une position a gauche", lineCommandsTable[c++], "h");
+        //Creating and filling CursorSubMenu
 
 
-*/
+        subMenuList[i++] = new EntryImp(i, "Placer le curseur au debut de la ligne    ", lineCommandsTable[c++], "o");
+        subMenuList[i++] = new EntryImp(i, "Avancer le curseur d une position a droite", lineCommandsTable[c++], "l");
+        subMenuList[i++] = new EntryImp(i, "Reculer le curseur d une position a gauche", lineCommandsTable[c++], "h");
+
         //filling main menu
-        mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere au debut de la ligne", lineCommandsTable[c++], "I");
-            /*
-            mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere a la fin de la ligne", lineCommandsTable[c++], "A");
-            Menu cursorSubMenu = new MenuImp(j + 1, "Menu Curseur                              ", "C", menuPrinterOutputPort);
-            for (int k = 0; k < subMenuList.length; k++) {
-                cursorSubMenu.add(subMenuList[k]);
-            }
-            mainMenuList[j++] = (Entry) cursorSubMenu;
-            mainMenuList[j++] = new EntryImp(j, "Remplacer le caractere sous le curseur   ", lineCommandsTable[c++], "r");
-            mainMenuList[j++] = new EntryImp(j, "Supprimer le caractere sous le curseur   ", lineCommandsTable[c++], "x");
-            mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere avant le curseur    ", lineCommandsTable[c++], "i");
-            mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere apres le curseur    ", lineCommandsTable[c++], "a");
-            mainMenuList[j++] = new EntryImp(j, "Supprimer tous les caracteres de la ligne", lineCommandsTable[c++], "dd");
 
-*/
-            for (int k = 0; k < mainMenuList.length; k++) {
-                this.myMenu.add(mainMenuList[k]);
-            }
+        mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere au debut de la ligne", lineCommandsTable[c++], "I");
+        mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere a la fin de la ligne", lineCommandsTable[c++], "A");
+        Menu cursorSubMenu = new MenuImp(j + 1, "Menu Curseur                              ", "C", menuPrinterOutputPort);
+        for (int k = 0; k < subMenuList.length; k++) {
+            cursorSubMenu.add(subMenuList[k]);
+        }
+        mainMenuList[j++] = (Entry) cursorSubMenu;
+        mainMenuList[j++] = new EntryImp(j, "Remplacer le caractere sous le curseur   ", lineCommandsTable[c++], "r");
+        mainMenuList[j++] = new EntryImp(j, "Supprimer le caractere sous le curseur   ", lineCommandsTable[c++], "x");
+        mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere avant le curseur    ", lineCommandsTable[c++], "i");
+        mainMenuList[j++] = new EntryImp(j, "Ajouter un caractere apres le curseur    ", lineCommandsTable[c++], "a");
+        mainMenuList[j++] = new EntryImp(j, "Supprimer tous les caracteres de la ligne", lineCommandsTable[c++], "dd");
+
+
+        for (int k = 0; k < mainMenuList.length; k++) {
+            this.myMenu.add(mainMenuList[k]);
+        }
 
     }
 
